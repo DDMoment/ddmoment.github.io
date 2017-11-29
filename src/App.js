111b219368b4136cd28af49f6742a3e514a2941e
ssysm <me@theeditorstudio.com>
2017-11-28 23:11:42 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">DD圈 - DD Moment</h1>
+        </header>
+        <p className="App-intro">
+          DD圈，分享大家的DD时光
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
